e537ee5f5d26a01a39ea2cc24171e1fab984ed77
r0ballis0n <rob.allison@anobletech.com>
2021-12-09 08:53:24 -0600

    Add files via upload
    Updates SAM Account Name in AD

---
+++ Update_SAMAccountName.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module ActiveDirectory
+$usersToChange = Get-ADUser -SearchBase “ou=OU,dc=local,dc=domain” -filter *
+# Grabs all the users you want and puts them in an array
+ 
+foreach ($user in $usersToChange) {
+ 
+### Get the samaccount name
+ 
+$oldLogon = Get-AdUser -identity $user.SamAccountName -Properties * | Select-Object -Expand SamAccountName;
+ 
+### Get the emailaddress
+$logonemail = Get-AdUser -identity $user.SamAccountName -Properties * | Select-Object -Expand Emailaddress;
+ 
+### New logon will be the emailaddress
+$newlogon = $logonemail.Tolower().Split("@")[0];
+ 
+Get-ADUser -identity $oldlogon | Set-AdUser -Replace @{userPrincipalName = $logonemail};
+Get-ADUser -identity $user.SamAccountName | Set-AdUser -Replace @{samaccountname = $newLogon};
+}
